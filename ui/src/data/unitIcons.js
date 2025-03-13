230f8408df49d9aa25db1c1574696f72ddef9ec6
Alec Jordan <alecbjordan@gmail.com>
2025-03-13 16:33:53 -0400

    Update unit icon strings.

--- ui/src/data/unitIcons.js
+++ ui/src/data/unitIcons.js
@@ -1,8 +1,88 @@
 export const unitIcons = [
 	{
 		name: "INFANTRY",
+		country: "United States",
+		unitIcon: new URL(
+			"@/assets/units/america-infantry.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "INFANTRY",
+		country: "United Kingdom",
+		unitIcon: new URL(
+			"@/assets/units/british-infantry.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "INFANTRY",
 		country: "Soviet Union",
 		unitIcon: new URL("@/assets/units/russia-infantry.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "INFANTRY",
+		country: "Germany",
+		unitIcon: new URL(
+			"@/assets/units/germany-infantry.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "INFANTRY",
+		country: "Japan",
+		unitIcon: new URL("@/assets/units/japan-infantry.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "ARTILLERY",
+		country: "United States",
+		unitIcon: new URL(
+			"@/assets/units/allies-artillery.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "ARTILLERY",
+		country: "United Kingdom",
+		unitIcon: new URL(
+			"@/assets/units/allies-artillery.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "ARTILLERY",
+		country: "Soviet Union",
+		unitIcon: new URL(
+			"@/assets/units/allies-artillery.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "ARTILLERY",
+		country: "Germany",
+		unitIcon: new URL(
+			"@/assets/units/germany-artillery.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "ARTILLERY",
+		country: "Japan",
+		unitIcon: new URL("@/assets/units/japan-artillery.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "TANK",
+		country: "United States",
+		unitIcon: new URL("@/assets/units/allies-tank.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "TANK",
+		country: "United Kingdom",
+		unitIcon: new URL("@/assets/units/allies-tank.png", import.meta.url)
 			.href,
 	},
 	{
@@ -12,9 +92,307 @@ export const unitIcons = [
 			.href,
 	},
 	{
+		name: "TANK",
+		country: "Germany",
+		unitIcon: new URL("@/assets/units/germany-tank.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "TANK",
+		country: "Japan",
+		unitIcon: new URL("@/assets/units/japan-tank.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "ANTI-AIRCRAFT",
+		country: "All",
+		unitIcon: new URL("@/assets/units/anti-aircraft.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "INDUSTRIAL-COMPLEX",
+		country: "All",
+		unitIcon: new URL(
+			"@/assets/units/industrial-complex.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "FIGHTER",
+		country: "United States",
+		unitIcon: new URL("@/assets/units/america-fighter.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "FIGHTER",
+		country: "United Kingdom",
+		unitIcon: new URL("@/assets/units/uk-fighter.png", import.meta.url)
+			.href,
+	},
+	{
 		name: "FIGHTER",
 		country: "Soviet Union",
 		unitIcon: new URL("@/assets/units/russia-fighter.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "FIGHTER",
+		country: "Germany",
+		unitIcon: new URL("@/assets/units/germany-fighter.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "FIGHTER",
+		country: "Japan",
+		unitIcon: new URL("@/assets/units/japan-fighter.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "BOMBER",
+		country: "United States",
+		unitIcon: new URL("@/assets/units/america-bomber.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "BOMBER",
+		country: "United Kingdom",
+		unitIcon: new URL("@/assets/units/uk-bomber.png", import.meta.url).href,
+	},
+	{
+		name: "BOMBER",
+		country: "Soviet Union",
+		unitIcon: new URL("@/assets/units/russia-bomber.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "BOMBER",
+		country: "Germany",
+		unitIcon: new URL("@/assets/units/germany-bomber.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "BOMBER",
+		country: "Japan",
+		unitIcon: new URL("@/assets/units/japan-bomber.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "BATTLESHIP",
+		country: "United States",
+		unitIcon: new URL(
+			"@/assets/units/america-battleship.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "BATTLESHIP",
+		country: "United Kingdom",
+		unitIcon: new URL(
+			"@/assets/units/allies-battleship.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "BATTLESHIP",
+		country: "Soviet Union",
+		unitIcon: new URL(
+			"@/assets/units/allies-battleship.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "BATTLESHIP",
+		country: "Germany",
+		unitIcon: new URL(
+			"@/assets/units/germany-battleship.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "BATTLESHIP",
+		country: "Japan",
+		unitIcon: new URL(
+			"@/assets/units/japan-battleship.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "DESTROYER",
+		country: "United States",
+		unitIcon: new URL(
+			"@/assets/units/allies-destroyer.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "BATTLESHIP",
+		country: "United Kingdom",
+		unitIcon: new URL(
+			"@/assets/units/allies-destroyer.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "BATTLESHIP",
+		country: "Soviet Union",
+		unitIcon: new URL(
+			"@/assets/units/allies-destroyer.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "BATTLESHIP",
+		country: "Germany",
+		unitIcon: new URL(
+			"@/assets/units/germany-destroyer.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "BATTLESHIP",
+		country: "Japan",
+		unitIcon: new URL("@/assets/units/japan-destroyer.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "DESTROYER",
+		country: "United States",
+		unitIcon: new URL(
+			"@/assets/units/allies-destroyer.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "DESTROYER",
+		country: "United Kingdom",
+		unitIcon: new URL(
+			"@/assets/units/allies-destroyer.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "DESTROYER",
+		country: "Soviet Union",
+		unitIcon: new URL(
+			"@/assets/units/allies-destroyer.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "DESTROYER",
+		country: "Germany",
+		unitIcon: new URL(
+			"@/assets/units/germany-destroyer.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "DESTROYER",
+		country: "Japan",
+		unitIcon: new URL("@/assets/units/japan-destroyer.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "AIRCRAFT-CARRIER",
+		country: "United States",
+		unitIcon: new URL("@/assets/units/america-ac.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "AIRCRAFT-CARRIER",
+		country: "United Kingdom",
+		unitIcon: new URL("@/assets/units/allies-ac.png", import.meta.url).href,
+	},
+	{
+		name: "AIRCRAFT-CARRIER",
+		country: "Soviet Union",
+		unitIcon: new URL("@/assets/units/allies-ac.png", import.meta.url).href,
+	},
+	{
+		name: "AIRCRAFT-CARRIER",
+		country: "Germany",
+		unitIcon: new URL("@/assets/units/germany-ac.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "AIRCRAFT-CARRIER",
+		country: "Japan",
+		unitIcon: new URL("@/assets/units/japan-ac.png", import.meta.url).href,
+	},
+	{
+		name: "TRANSPORT",
+		country: "United States",
+		unitIcon: new URL(
+			"@/assets/units/allies-transport.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "TRANSPORT",
+		country: "United Kingdom",
+		unitIcon: new URL(
+			"@/assets/units/allies-transport.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "TRANSPORT",
+		country: "Soviet Union",
+		unitIcon: new URL(
+			"@/assets/units/allies-transport.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "TRANSPORT",
+		country: "Germany",
+		unitIcon: new URL(
+			"@/assets/units/germany-transport.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "TRANSPORT",
+		country: "Japan",
+		unitIcon: new URL("@/assets/units/japan-transport.png", import.meta.url)
+			.href,
+	},
+	{
+		name: "SUBMARINE",
+		country: "United States",
+		unitIcon: new URL(
+			"@/assets/units/allies-submarine.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "SUBMARINE",
+		country: "United Kingdom",
+		unitIcon: new URL(
+			"@/assets/units/allies-submarine.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "SUBMARINE",
+		country: "Soviet Union",
+		unitIcon: new URL(
+			"@/assets/units/allies-submarine.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "SUBMARINE",
+		country: "Germany",
+		unitIcon: new URL(
+			"@/assets/units/germany-submarine.png",
+			import.meta.url
+		).href,
+	},
+	{
+		name: "SUBMARINE",
+		country: "Japan",
+		unitIcon: new URL("@/assets/units/japan-submarine.png", import.meta.url)
 			.href,
 	},
 ];
