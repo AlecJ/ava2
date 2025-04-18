08f684df1a8ab52f9038a9ebed62d2c0ed30dcff
Alec Jordan <alecbjordan@gmail.com>
2025-04-18 15:19:02 -0400

    Trigger github action.

--- ui/src/services/api.js
+++ ui/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const API = axios.create({
-	baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+	baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
 	timeout: process.env.NODE_ENV === "development" ? 10000 : 5000,
 });
 
